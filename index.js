806d39ea62ab32238244dd776d40ed45148a3754
6Leoo6 <77102589+6Leoo6@users.noreply.github.com>
2023-02-25 03:15:38 +0100

    design change

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function scrollDown() {
-    document.getElementById('famous-words-sec').scrollIntoView()
-}
+//document.querySelectorAll('.quote').forEach((e) => {
+//    var top = Math.floor(Math.random()*40)+5
+//    var left = Math.floor(Math.random()*50)+5
+//    var opacity = (Math.floor(Math.random()*10)+4)/10
+//    e.style.marginTop = `${top}vh`
+//    e.style.marginLeft = `${left}vw`
+//    e.style.opacity = opacity
+//    console.log(opacity)
+//})
